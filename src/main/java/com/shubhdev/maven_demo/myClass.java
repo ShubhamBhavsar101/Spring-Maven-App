021b287aac3ea0ddf5293bdc1f289043febea9f7
Shubham Bhavsar <Shubhamcbhavsar198@gmail.com>
2025-04-25 13:37:06 +0530

    Added Maven Project

---
+++ src/main/java/com/shubhdev/maven_demo/myClass.java
@@ -0,0 +1,12 @@
+package com.shubhdev.maven_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class myClass {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
+    }
+}
